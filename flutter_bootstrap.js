b2cf985ed5f16c5c9b2c8230d749e3c57fb743c0
Allan Furukita <furukitaallan@gmail.com>
2025-07-01 12:15:09 -0300

    Deploy automatizado - atualizacao build web e cache

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "989394625"
+    serviceWorkerVersion: "1682070346"
   }
 });
